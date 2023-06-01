88b3d19f4afdee0b19d810bdabbeaf0fe09239a6
Elijah Lascurain <elijahlascurain95@gmail.com>
2023-06-01 15:16:32 -0700

    Changed movement script to have a fixed update and to allow for the input script to set movement direction.

--- Assets/Scripts/Movement/CharacterMovement.cs
+++ Assets/Scripts/Movement/CharacterMovement.cs
@@ -11,12 +11,20 @@ public class CharacterMovement : MonoBehaviour
     private float _jumpForce = 20;
     private float _jumpAngleMod = 0.25f;
 
+    //This variable changes depending on what the input script reads
+    private int _moveDirection = 0;
+    public int MoveDirection 
+    { 
+        get { return _moveDirection; }
+        set { _moveDirection = value; }
+    }
+
     void Awake()
     {
         _playerRb = GetComponent<Rigidbody>();
     }
 
-    public void CharacterMove(int _moveDirection)
+    public void CharacterMove()
     {
         //_moveDirection is a +/-1 that determines whether the player is going left or right
         float _horizontalTranslation = _moveDirection * _walkSpeed;
@@ -25,12 +33,17 @@ public class CharacterMovement : MonoBehaviour
         transform.Translate(_horizontalTranslation, 0, 0);
     }
 
-    public void CharacterJump(int _moveDirection)
+    //Input script calls this function once since it is a single application of force
+    public void CharacterJump()
     {
-        //singular application of complete vertical force
         if(_moveDirection == 0)
             _playerRb.AddForce(Vector2.up * _jumpForce, ForceMode.Impulse);
         else
             _playerRb.AddForce(new Vector2(_moveDirection * _jumpAngleMod, 1) * _jumpForce, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        CharacterMove();
     }
 }
