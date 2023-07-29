b0967c0f714c1a1228e6962ae080cb357c5c706b
Elijah Lascurain <elijahlascurain95@gmail.com>
2023-07-28 18:53:40 -0700

    lool

--- Assets/Scripts/Rhythm Meter/RhythmBarBehavior.cs
+++ Assets/Scripts/Rhythm Meter/RhythmBarBehavior.cs
@@ -5,22 +5,25 @@ using DG.Tweening;
 
 public class RhythmBarBehavior : MonoBehaviour
 {
-    private Vector3 _downBeatEndPosition = new Vector3(814, 61, 0);
+    private GameObject _downBeatEndObject;
 
     void Awake()
     {
-        StartCoroutine(Move(transform.position, _downBeatEndPosition, 2.5f));
+        _downBeatEndObject = GameObject.Find("Center Visual");
+        StartCoroutine(Move(transform.position, _downBeatEndObject, 2.5f));
         Invoke("Destroy", 2.5f);
     }
 
-    private IEnumerator Move(Vector3 beginPos, Vector3 endPos, float time)
+    
+    private IEnumerator Move(Vector3 beginPos, GameObject endPos, float time)
     {
         for (float t = 0; t < 1; t += Time.deltaTime / time)
         {
-            transform.position = Vector3.Lerp(beginPos, endPos, t);
+            transform.position = Vector3.Lerp(beginPos, endPos.transform.position, t);
             yield return null;
         }
     }
+    
 
     private void Destroy()
     {
